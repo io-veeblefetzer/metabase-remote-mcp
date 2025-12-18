730fee158f9e37bd71ee370cd8b1be51c61bd0a1
Gie Spaepen <giespaepen@outlook.com>
2025-12-18 10:27:56 +0100

    chore: Added base framework and dependencies
    Addes files from spring initializr

---
+++ src/main/java/io/veeblefetzer/remote_mcp_server/RemoteMcpServerApplication.java
@@ -0,0 +1,13 @@
+package io.veeblefetzer.remote_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/veeblefetzer/remote_mcp_server/RemoteMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.veeblefetzer.remote_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
